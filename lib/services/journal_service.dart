802b52528c2760dc329081edea6b4b72c7a26eac
Leonardo Silva <leonardosilvaif@hotmail.com>
2026-01-04 14:53:36 -0300

    feat: using interceptors (http_interceptor) to log request/response operations

--- lib/services/journal_service.dart
+++ lib/services/journal_service.dart
@@ -1,10 +1,15 @@
 import 'dart:convert';
 
+import 'package:flutter_webapi_first_course/services/http_interceptors.dart';
 import 'package:http/http.dart' as http;
+import 'package:http_interceptor/http/intercepted_client.dart';
 
 class JournalService {
   static const String url = "http://192.168.1.6:3000/";
   static const String resource = "learnhttp";
+
+  http.Client client =
+      InterceptedClient.build(interceptors: [HttpInterceptors()]);
 
   String getUrl() {
     return "$url$resource";
@@ -16,7 +21,7 @@ class JournalService {
     // print("completeUrl = $completeUrl");
     // http.post(Uri.parse(completeUrl), body: {"content": content});
     // print(response.statusCode);
-    http.post(
+    client.post(
       Uri.parse(getUrl()),
       headers: {"Content-Type": "application/json"},
       body: json.encode({"content": content}),
@@ -24,8 +29,7 @@ class JournalService {
   }
 
   Future<String> get() async {
-    http.Response response = await http.get(Uri.parse(getUrl()));
-    print(response.body);
+    http.Response response = await client.get(Uri.parse(getUrl()));
     return response.body;
   }
 }
