ae25181d86beeb01dd4e76c18dc52c6665c2d6b9
Leonardo Silva <leonardosilvaif@hotmail.com>
2026-01-05 11:04:19 -0300

    feat: using parameters in named routes

--- lib/main.dart
+++ lib/main.dart
@@ -27,20 +27,21 @@ class MyApp extends StatelessWidget {
             backgroundColor: Colors.black,
             titleTextStyle: TextStyle(color: Colors.white),
             actionsIconTheme: IconThemeData(color: Colors.white),
+            iconTheme: IconThemeData(color: Colors.white),
           ),
           textTheme: GoogleFonts.bitterTextTheme()),
       darkTheme: ThemeData.dark(),
       themeMode: ThemeMode.light,
-      initialRoute: "add-journal",
+      initialRoute: "home",
       routes: {
         "home": (context) => const HomeScreen(),
-        "add-journal": (context) => AddJournalScreen(
-              journal: Journal(
-                  content: "content",
-                  id: "id",
-                  createdAt: DateTime.now(),
-                  updatedAt: DateTime.now()),
-            ),
+      },
+      onGenerateRoute: (settings) {
+        if (settings.name == "add-journal") {
+          final Journal journal = settings.arguments as Journal;
+          return MaterialPageRoute(
+              builder: (context) => AddJournalScreen(journal: journal));
+        }
       },
     );
   }
