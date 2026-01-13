5b304fa9cf4e727df6193ba600d0f14bde2767d8
Leonardo Silva <leonardosilvaif@hotmail.com>
2026-01-13 13:42:25 -0300

    feat: using library shared_preferences to store response.body info, i.e. token, id, and email

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -4,6 +4,7 @@ import 'dart:io';
 import 'package:flutter_webapi_first_course/services/http_interceptors.dart';
 import 'package:http/http.dart' as http;
 import 'package:http_interceptor/http/intercepted_client.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthService {
   //TODO: extract url to be used by all services
@@ -27,12 +28,36 @@ class AuthService {
       throw HttpException(response.body);
     }
 
+    saveUserInfo(response.body);
+
     return true;
   }
 
-  register({required String email, required String password}) async {
+  Future<void> register(
+      {required String email, required String password}) async {
     http.Response response = await client.post(Uri.parse('${url}register'),
         body: {'email': email, 'password': password});
+
+    if (response.statusCode != 201) {
+      throw HttpException(response.body);
+    }
+
+    saveUserInfo(response.body);
+  }
+
+  saveUserInfo(String body) async {
+    Map<String, dynamic> map = json.decode(body);
+
+    String token = map['accessToken'];
+    String email = map['user']['email'];
+    int id = map['user']['id'];
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString('accessToken', token);
+    prefs.setString('email', email);
+    prefs.setInt('id', id);
+
+    //print('$token\n$email\n$id');
   }
 }
 
